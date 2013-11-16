234eeb031db4687ccf9c5c55a0b296d5f5659b94
Richard Crowley <r@rcrowley.org>
2013-11-16 18:00:37 +0000

    Test SSL with our own cert, not Betable's.

--- server_test.go
+++ server_test.go
@@ -4,11 +4,8 @@ import "testing"
 
 func TestServerCATLS(t *testing.T) {
 	s := NewServer("", NotFoundHandler{})
-	s.CA("/etc/ssl/certs/betable-ca.pem")
-	s.TLS(
-		"/etc/ssl/certs/betable-internal.crt",
-		"/etc/ssl/private/betable-internal.key",
-	)
+	s.CA("test.crt")
+	s.TLS("test.crt", "test.key")
 	if nil == s.TLSConfig.Certificates || 1 != len(s.TLSConfig.Certificates) {
 		t.Fatal("no Certificates")
 	}
